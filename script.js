090a3a9c725e098ab9fa78ef101ef16f29db9040
Joao Artur <joaoartur4590@gmail.com>
2022-03-11 11:49:00 -0300

    finished

--- script.js
+++ script.js
@@ -5,32 +5,24 @@ let messages = document.querySelector(".messages");
 let machineChoise;
 let humanChoise;
 
+let hpoints = 0;
+let mpoints = 0;
+
 let choisePaper = document.querySelector(".playerChoisePaper");
 let choiseRock = document.querySelector(".playerChoiseRock");
 let choiseScissors = document.querySelector(".playerChoiseScissors");
-// 0:draw/ 1:human victory
-const calculateMatch = (humanChoise, machineChoise)=>{
-if(humanChoise === )
-}
 
-const sortear = () => {
-  console.log(Math.floor(Math.random() * 4));
-};
+let mchoisePaper = document.querySelector(".machineChoisePaper");
+let mchoiseRock = document.querySelector(".machineChoiseRock");
+let mchoiseScissors = document.querySelector(".machineChoiseScissors");
 
-// 1 -paper/ 2- rock/ 3-scissors
-const play = (choise) => {
-  machineChoise = sortear();
-  humanChoise = choise;
-
-calculateMatch(humanChoise, machineChoise)
-  if
-};
+let humanPoints = document.querySelector(".humanPoints");
+let machinePoints = document.querySelector(".machinePoints");
 
 const getPlayerName = () => {
   playerName = prompt("Qual o seu nome?");
-  console.log(playerName);
+
   while (playerName === "" || playerName === null) {
-    console.log(playerName);
     playerName = prompt("Qual o seu nome?");
     if (playerName !== "" && playerName !== null) {
       break;
@@ -40,23 +32,97 @@ const getPlayerName = () => {
 
 getPlayerName();
 nameOfPlayer.innerHTML = playerName;
-messages.innerText = `Olá ${playerName}, bem-vindo!`;
+messages.innerText = `Olá ${playerName}, bem-vindo! Escolha uma das três opções para jogar.`;
+
+// 0:draw/ 1:human victory/ 2: human defeat
+const calculateMatch = (humanChoise, machineChoise) => {
+  if (humanChoise === 1 && machineChoise === 1) {
+    return 0;
+  } else if (humanChoise === 1 && machineChoise === 2) {
+    return 1;
+  } else if (humanChoise === 1 && machineChoise === 3) {
+    return 2;
+  } else if (humanChoise === 2 && machineChoise === 1) {
+    return 2;
+  } else if (humanChoise === 2 && machineChoise === 2) {
+    return 0;
+  } else if (humanChoise === 2 && machineChoise === 3) {
+    return 1;
+  } else if (humanChoise === 3 && machineChoise === 1) {
+    return 1;
+  } else if (humanChoise === 3 && machineChoise === 2) {
+    return 2;
+  } else if (humanChoise === 3 && machineChoise === 3) {
+    return 0;
+  }
+};
+
+const sortear = () => {
+  return Math.floor(Math.random() * 3 + 1);
+};
+
+// 1 -paper/ 2- rock/ 3-scissors
+const play = (choise) => {
+  machineChoise = sortear();
+  if (machineChoise === 1) {
+    mchoiseScissors.classList.remove("machineSelected");
+    mchoiseRock.classList.remove("machineSelected");
+    mchoisePaper.classList.remove("machineSelected");
+
+    mchoisePaper.classList.add("machineSelected");
+  }
+  if (machineChoise === 2) {
+    mchoiseScissors.classList.remove("machineSelected");
+    mchoiseRock.classList.remove("machineSelected");
+    mchoisePaper.classList.remove("machineSelected");
+
+    mchoiseRock.classList.add("machineSelected");
+  }
+  if (machineChoise === 3) {
+    mchoiseScissors.classList.remove("machineSelected");
+    mchoiseRock.classList.remove("machineSelected");
+    mchoisePaper.classList.remove("machineSelected");
+
+    mchoiseScissors.classList.add("machineSelected");
+  }
+  humanChoise = choise;
+  console.log(humanChoise, machineChoise);
+};
+
+const winner = () => {
+  let result = calculateMatch(humanChoise, machineChoise);
+  console.log("result", result);
+  if (result === 0) {
+    messages.innerText = "Deu empate!";
+  } else if (result === 1) {
+    hpoints++;
+    messages.innerText = `Parabéns ${playerName}, você venceu!`;
+
+    humanPoints.innerText = `${hpoints}`;
+  } else if (result === 2) {
+    mpoints++;
+    machinePoints.innerText = `${mpoints}`;
+    messages.innerText = `A máquina venceu :( !`;
+  }
+};
 
 choisePaper.addEventListener("click", (e) => {
   e.preventDefault();
-  console.log("paper");
-  sortear();
+
   play(1);
+  winner(humanChoise, machineChoise);
 });
 
 choiseRock.addEventListener("click", (e) => {
   e.preventDefault();
-  console.log("rock");
+
   play(2);
+  winner(humanChoise, machineChoise);
 });
 
 choiseScissors.addEventListener("click", (e) => {
   e.preventDefault();
-  console.log("scissors");
+
   play(3);
+  winner(humanChoise, machineChoise);
 });
